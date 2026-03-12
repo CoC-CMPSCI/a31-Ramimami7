e97ef84501e5488609addf4f610a7e224473e17c
Ramimami7 <145726067+Ramimami7@users.noreply.github.com>
2026-03-11 19:51:08 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,18 +8,28 @@ int main()
   const double ClassCCost = 9.00;
 
   // TODO: Declare integer variables for tickets sold for each class
-
+  int ticketsA, ticketsB, ticketsC;
+  ticketsA = 10;
+  ticketsB = 20;
+  ticketsC = 15;
   // TODO: Declare double variables for total sales per class and grand total
-
+  double totalA, totalB, totalC, total;
   // TODO: Prompt user and read number of Class A tickets sold
-
+  
+  cout << "Tickets sold for Class A " << ticketsA << " Sales Price for A "<< ClassACost << endl;
   // TODO: Prompt user and read number of Class B tickets sold
-
+  
+  cout << "Tickets sold for Class B  " << ticketsB << "  Sales Price for B "<< ClassBCost << endl;
   // TODO: Prompt user and read number of Class C tickets sold
-
+  
+  cout << "Tickets sold for Class C  " << ticketsC << "  Sales Price for C "<< ClassCCost << endl;
   // TODO: Calculate sales for each class
-
+  totalA = ticketsA * ClassACost;
+  totalB = ticketsB * ClassBCost;
+  totalC = ticketsC * ClassCCost;
+  
   // TODO: Calculate total sales
+  total = totalA + totalB + totalC
 
   cout << setprecision(2) << fixed;
   cout << "Tickets sold for Class A " << ticketsA << " Sales Price for A   " << totalA << endl;
